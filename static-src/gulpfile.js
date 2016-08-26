ba43e4d82dbbacd2392743455545625d00da6621
MP Droog <mpdroog@icloud.com>
2016-08-26 20:20:16 +0200

    Feature. Shrink HTML as well

--- static-src/gulpfile.js
+++ static-src/gulpfile.js
@@ -9,6 +9,7 @@ var fs = require('fs');
 var zopfli = require("gulp-zopfli");
 var merge = require('merge-stream');
 var util = require('gulp-util');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('js', function() {
   return gulp.src(['js/*.js'])
@@ -29,20 +30,15 @@ gulp.task('css', ['static'], function() {
     }))
     .pipe(minifyCSS({'keepSpecialComments': 0}))
     .pipe(gulp.dest('../build'));
-
-    /*var extra = gulp.src(['assets/typicons/typicons.min.css', 'assets/animate.css', 'assets/extra.css'])
-    .pipe(concat('extra.' + deployVersion + '.css'))
-    .pipe(minifyCSS({'keepSpecialComments': 0}))
-    .pipe(gulp.dest('www/assets/typicons'));
-
-    return merge(base, extra);*/
     return base;
 });
 
 gulp.task('static', function() {
   var base = gulp.src([
     'index.html'
-  ]).pipe(gulp.dest('../build'));
+  ]).pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+  .pipe(gulp.dest('../build'));
+
   var imgs = gulp.src([
     'images/**/*'
   ]).pipe(gulp.dest('../build/images'));
