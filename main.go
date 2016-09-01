2d7d70f55d161e93550af456abfda87f5c965202
MP Droog <mpdroog@icloud.com>
2016-09-01 10:39:55 +0200

    go fmt && missing newline on missing ENV-var

--- main.go
+++ main.go
@@ -4,47 +4,47 @@
 package main
 
 import (
-    "os"
-	"net/http"
-	"fmt"
 	"flag"
-    "gopkg.in/mailgun/mailgun-go.v1"
-    "gopkg.in/validator.v2"
-    "github.com/gorilla/schema"
+	"fmt"
+	"github.com/gorilla/schema"
+	"gopkg.in/mailgun/mailgun-go.v1"
+	"gopkg.in/validator.v2"
+	"net/http"
+	"os"
 )
 
 var (
-	verbose bool
-    mailgunDomain string
-    mailgunApi string
-    mailgunApiPub string
+	verbose       bool
+	mailgunDomain string
+	mailgunApi    string
+	mailgunApiPub string
 )
 
-func email(w http.ResponseWriter, r *http.Request) {    
-    msg := new(Email)
-    dec := schema.NewDecoder()
-    if e := dec.Decode(msg, r.PostForm); e != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte("Sorry undecodable input."))
-        fmt.Printf("ERR:email:decode: %s\n", e)
-        return
-    }
-    if e := validator.NewValidator().Validate(msg); e != nil {
-        w.WriteHeader(http.StatusUnprocessableEntity)
-        w.Write([]byte("Sorry invalid input."))
-        fmt.Printf("ERR:email:validate: %s\n", e)
-        return
-    }
+func email(w http.ResponseWriter, r *http.Request) {
+	msg := new(Email)
+	dec := schema.NewDecoder()
+	if e := dec.Decode(msg, r.PostForm); e != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Sorry undecodable input."))
+		fmt.Printf("ERR:email:decode: %s\n", e)
+		return
+	}
+	if e := validator.NewValidator().Validate(msg); e != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("Sorry invalid input."))
+		fmt.Printf("ERR:email:validate: %s\n", e)
+		return
+	}
 
-    mg := mailgun.NewMailgun(mailgunDomain, mailgunApi, mailgunApiPub)
-    message := mailgun.NewMessage("noreply@rootdev.nl", "Contact request", "From=" + msg.Email + "\n\n" + msg.Body, "rootdev@gmail.com")
-    if _, idx, e := mg.Send(message); e != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte("Sorry failed sending email."))
-        fmt.Printf("ERR:email:send(%d): %s\n", idx, e)
-        return
-    }
-    w.Write([]byte("Sent email."))
+	mg := mailgun.NewMailgun(mailgunDomain, mailgunApi, mailgunApiPub)
+	message := mailgun.NewMessage("noreply@rootdev.nl", "Contact request", "From="+msg.Email+"\n\n"+msg.Body, "rootdev@gmail.com")
+	if _, idx, e := mg.Send(message); e != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Sorry failed sending email."))
+		fmt.Printf("ERR:email:send(%d): %s\n", idx, e)
+		return
+	}
+	w.Write([]byte("Sent email."))
 }
 
 func main() {
@@ -52,31 +52,31 @@ func main() {
 	flag.BoolVar(&verbose, "v", false, "Verbose-mode (log more)")
 	flag.Parse()
 
-    mailgunDomain = os.Getenv("MAILGUN_DOMAIN")
-    mailgunApi = os.Getenv("MAILGUN_APIKEY")
-    mailgunApiPub = os.Getenv("MAILGUN_PUBLICAPIKEY")
+	mailgunDomain = os.Getenv("MAILGUN_DOMAIN")
+	mailgunApi = os.Getenv("MAILGUN_APIKEY")
+	mailgunApiPub = os.Getenv("MAILGUN_PUBLICAPIKEY")
 
-    if mailgunDomain == "" {
-        fmt.Printf("ERR:main:env: Missing MAILGUN_DOMAIN")
-        return
-    }
-    if mailgunApi == "" {
-        fmt.Printf("ERR:main:env: Missing MAILGUN_APIKEY")
-        return
-    }
-    if mailgunApiPub == "" {
-        fmt.Printf("ERR:main:env: Missing MAILGUN_PUBLICAPIKEY")
-        return
-    }
+	if mailgunDomain == "" {
+		fmt.Printf("ERR:main:env: Missing MAILGUN_DOMAIN\n")
+		return
+	}
+	if mailgunApi == "" {
+		fmt.Printf("ERR:main:env: Missing MAILGUN_APIKEY\n")
+		return
+	}
+	if mailgunApiPub == "" {
+		fmt.Printf("ERR:main:env: Missing MAILGUN_PUBLICAPIKEY\n")
+		return
+	}
 
 	fs := http.FileServer(http.Dir("build"))
-  	http.Handle("/", fs)
-    http.HandleFunc("/action/email", email)
+	http.Handle("/", fs)
+	http.HandleFunc("/action/email", email)
 
-    if verbose {
-	    fmt.Printf("Listening on %s\n", listen)
+	if verbose {
+		fmt.Printf("Listening on %s\n", listen)
 	}
-    if e := http.ListenAndServe(listen, nil); e != nil {
-    	panic(e)
-    }
+	if e := http.ListenAndServe(listen, nil); e != nil {
+		panic(e)
+	}
 }
--- validate.go
+++ validate.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"regexp"
+	"errors"
 	"gopkg.in/validator.v2"
 	"reflect"
-	"errors"
+	"regexp"
 )
 
 type Email struct {
-    Body string
-    Email string `valid:"email"`
+	Body  string
+	Email string `valid:"email"`
 }
 
 // https://github.com/asaskevich/govalidator/blob/136d678f787d7907b57e992f97346acc83034a57/patterns.go
 const (
-	EmailRegex          string = "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
+	EmailRegex string = "^(((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$"
 )
 
 var rxEmail = regexp.MustCompile(EmailRegex)
 
 func init() {
-    validator.SetValidationFunc("email", validateEmail)
+	validator.SetValidationFunc("email", validateEmail)
 }
 
 // Very simple validator with parameter
 func validateEmail(v interface{}, param string) error {
-    st := reflect.ValueOf(v)
-    if st.Kind() != reflect.String {
-        return validator.ErrUnsupported
-    }
-    if !rxEmail.MatchString(st.String()) {
-        return errors.New("value must be email")
-    }
-    return nil
+	st := reflect.ValueOf(v)
+	if st.Kind() != reflect.String {
+		return validator.ErrUnsupported
+	}
+	if !rxEmail.MatchString(st.String()) {
+		return errors.New("value must be email")
+	}
+	return nil
 }
