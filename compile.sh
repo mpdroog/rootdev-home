665b33bcfda45351d03a1b2e5f4935fe00c81a21
MP Droog <mpdroog@icloud.com>
2016-09-30 07:29:55 +0200

    Bugfix. Missing gulp build in compile-script

--- compile.sh
+++ compile.sh
@@ -2,5 +2,10 @@
 # Cross-compile for container
 set +x
 env GOOS=linux GOARCH=amd64 go build
+
+cd static-src
+node_modules/gulp/bin/gulp.js
+cd -
+
 docker build . -t "mpdroog/rootdev"
 docker push mpdroog/rootdev
