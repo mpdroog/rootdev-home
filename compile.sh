94fba64c86d18d1113c3bd373b3c444446373650
MP Droog <mpdroog@icloud.com>
2016-10-07 14:03:27 +0200

    Bugfix. Set ELF ABI to 003 for FreeBSD to execute the binary (same as brandelf -t Linux on FreeBSD)

--- compile.sh
+++ compile.sh
@@ -2,6 +2,8 @@
 # Cross-compile for container
 set +x
 env GOOS=linux GOARCH=amd64 go build
+# Set ELF ABI to 003 to run under FreeBSD
+echo -n $'\003' | dd bs=1 count=1 seek=7 conv=notrunc of=./rootdev
 
 cd static-src
 node_modules/gulp/bin/gulp.js
