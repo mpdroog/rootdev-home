d5511db624459c3cf15ae7040366ce72448df539
MP Droog <mpdroog@icloud.com>
2017-01-11 11:55:58 +0100

    Feature. Install latest NodeJS lib with yarn and show what command is ran

--- compile.sh
+++ compile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Cross-compile for container
-set +x
+set -e
+set -x
 env GOOS=linux GOARCH=amd64 go build
 # Set ELF ABI to 003 to run under FreeBSD
 echo -n $'\003' | dd bs=1 count=1 seek=7 conv=notrunc of=./rootdev
 
 cd static-src
+yarn
 node_modules/gulp/bin/gulp.js
 cd -
 
